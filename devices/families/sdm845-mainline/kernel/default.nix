cf02cb5a9d8179ba141da91a9b7d8411c5c61031
matthewcroughan <matt@croughan.sh>
2023-04-04 00:47:15 +0100

    sdm845-mainline: kernel 6.2.0 -> 6.3.0-dev

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -5,14 +5,15 @@
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "6.2.0";
+  version = "6.3-dev";
+  modDirVersion = "6.3.0-rc1";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "sdm845-${version}";
-    hash = "sha256-UaCBjFG0TuyZhISO6H6T0oNiBft6rYJGVHZnQgal+HQ=";
+    rev = "sdm845/${version}";
+    hash = "sha256-rRG7ixH98S/WiCqoQH6M1O//DIzkNVDDnQloiBThTkw=";
   };
 
   isModular = false;
